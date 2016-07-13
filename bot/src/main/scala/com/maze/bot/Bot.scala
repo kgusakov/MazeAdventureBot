66e251cf5c2efe01d34a864a885c0f5dd60324d6
Kirill Gusakov <kgusakov@gmail.com>
2016-07-13 22:11:07 +0300

    Added fail safety with telegram communications

--- bot/src/main/scala/com/maze/bot/Bot.scala
+++ bot/src/main/scala/com/maze/bot/Bot.scala
@@ -20,7 +20,7 @@ import scala.concurrent.duration._
 import scala.io.Source
 import scalaz.Scalaz._
 
-object Bot extends App with LazyLogging  {
+object Bot extends App with LazyLogging {
 
   private val updateIdProp: String = "update_id"
   var updateId = PropsStore.properties.getProperty(updateIdProp, "0").toInt
@@ -43,48 +43,59 @@ object Bot extends App with LazyLogging  {
     else Left("Wrong direction")
   }
 
-  while(true) {
-    Await.result(TelegramApiClient.getUpdates(updateId), 1 minute) match {
-          case Some(updates) => {
-            updates.filter(_.message.isCommand).foreach { update =>
-              logger.info("Received update: " + update)
-              update.message match {
-                case Message(_, from, chat, _, Some(text), _) if text.startsWith("/new") =>
-                  gameManager ! NewGame(chat.id, from.id)
-                case Message(_, from, chat, _, Some(text), _) if text.startsWith("/join") =>
-                  gameManager ! JoinGame(chat.id, from.id)
-                case Message(_, from, chat, _, Some(text), _) if text.startsWith("/start") =>
-                  gameManager ! StartGame(chat.id, from.id)
-                case Message(_, from, chat, _, Some(text), _) if text.startsWith("/end") =>
-                  gameManager ! EndGame(chat.id, from.id)
-                case Message(_, from, chat, _, Some(text), _) if text.startsWith("/move") =>
-                  toMove(text) match {
-                    case Left(errorMessage) => TelegramApiClient.sendMessage(SendMessage(chat.id, errorMessage))
-                    case Right(direction) => gameManager ! MoveAction(chat.id, from.id, direction)
-                  }
-                case message@Message(_, _, _, _, _, _) =>
-                  logger.warn(message.toString)
-              }
-            }
-            val nextUpdateId = updates.map(_.updateId).fold(0)((a, b) => math.max(a, b))
-            if (nextUpdateId >= updateId) {
-              updateId = nextUpdateId + 1
-              PropsStore.save(updateIdProp, updateId.toString)
-              logger.debug(updateId.toString)
+  while (true) {
+    try {
+      Await.result(TelegramApiClient.getUpdates(updateId), 5 minute) match {
+        case Some(updates) => {
+          updates.filter(_.message.isCommand).foreach { update =>
+            logger.info("Received update: " + update)
+            update.message match {
+              case Message(_, from, chat, _, Some(text), _) if text.startsWith("/new") =>
+                gameManager ! NewGame(chat.id, from.id)
+              case Message(_, from, chat, _, Some(text), _) if text.startsWith("/join") =>
+                gameManager ! JoinGame(chat.id, from.id)
+              case Message(_, from, chat, _, Some(text), _) if text.startsWith("/start") =>
+                gameManager ! StartGame(chat.id, from.id)
+              case Message(_, from, chat, _, Some(text), _) if text.startsWith("/end") =>
+                gameManager ! EndGame(chat.id, from.id)
+              case Message(_, from, chat, _, Some(text), _) if text.startsWith("/move") =>
+                toMove(text) match {
+                  case Left(errorMessage) => TelegramApiClient.sendMessage(SendMessage(chat.id, errorMessage))
+                  case Right(direction) => gameManager ! MoveAction(chat.id, from.id, direction)
+                }
+              case message@Message(_, _, _, _, _, _) =>
+                logger.warn(message.toString)
             }
           }
-          case None =>
+          val nextUpdateId = updates.map(_.updateId).fold(0)((a, b) => math.max(a, b))
+          if (nextUpdateId >= updateId) {
+            updateId = nextUpdateId + 1
+            PropsStore.save(updateIdProp, updateId.toString)
+            logger.debug(updateId.toString)
+          }
         }
+        case None =>
+      }
+    } catch {
+      _ =>
     }
 
+  }
+
   case class NewGame(chatId: Int, userId: Int)
+
   case class JoinGame(chatId: Int, userId: Int)
+
   case class StartGame(chatId: Int, userId: Int)
+
   case class EndGame(chatId: Int, userId: Int)
+
   case class MoveAction(chatId: Int, userId: Int, direction: Direction)
+
   case class WinGame(chatId: Int, snapshot: Maze)
 
   case class PendingGame(chatId: Int, author: Int, players: mutable.ArrayBuffer[Int])
+
   case class ActiveGame(author: Int, game: ActorRef)
 
   class GameManager extends Actor with ActorLogging {
@@ -97,43 +108,45 @@ object Bot extends App with LazyLogging  {
     override def receive: Receive = {
       case NewGame(chatId, userId) =>
         if ((pendingGames contains chatId) || (activeGames contains chatId))
-          sendMessage (SendMessage(chatId, "This chat already have one game"))
+          sendMessage(SendMessage(chatId, "This chat already have one game"))
         else {
           pendingGames += chatId -> PendingGame(chatId, userId, ArrayBuffer(userId))
-          sendMessage (SendMessage(chatId, "Game successfully created, waiting for players"))
+          sendMessage(SendMessage(chatId, "Game successfully created, waiting for players"))
         }
       case JoinGame(chatId, userId) =>
         if (pendingGames contains chatId) {
           pendingGames(chatId).players += userId
-          sendMessage (SendMessage(chatId, "Welcome to game! Waiting for other players"))
+          sendMessage(SendMessage(chatId, "Welcome to game! Waiting for other players"))
         } else if (activeGames contains chatId) {
-          sendMessage (SendMessage(chatId, "Game already started, sorry"))
+          sendMessage(SendMessage(chatId, "Game already started, sorry"))
         } else
-          sendMessage (SendMessage(chatId, "There are no any games to join in current chat"))
+          sendMessage(SendMessage(chatId, "There are no any games to join in current chat"))
       case StartGame(chatId, userId) =>
         if (pendingGames contains chatId) {
           if (userId == pendingGames(chatId).author) {
-            sendMessage (SendMessage(chatId, "Game started, let it bleed!"))
+            sendMessage(SendMessage(chatId, "Game started, let it bleed!"))
             activeGames += chatId -> {
               val game = pendingGames(chatId)
               ActiveGame(userId, context.system.actorOf(Props(new GameMaster(chatId, game.players.toSet))))
             }
           } else {
-            sendMessage (SendMessage(chatId, "Only creator of the game can start it"))
+            sendMessage(SendMessage(chatId, "Only creator of the game can start it"))
           }
         }
         else
-          sendMessage (SendMessage(chatId, "There are no pending games to start"))
+          sendMessage(SendMessage(chatId, "There are no pending games to start"))
       case EndGame(chatId, userId) =>
         if ((pendingGames contains chatId) || (activeGames contains chatId))
           if (pendingGames.get(chatId).fold(false)(_.author == userId) ||
             activeGames.get(chatId).fold(false)(_.author == userId)) {
-            activeGames.get(chatId).foreach{ _.game ! PoisonPill }
+            activeGames.get(chatId).foreach {
+              _.game ! PoisonPill
+            }
             activeGames -= chatId
             pendingGames -= chatId
-            sendMessage (SendMessage(chatId, "Game finished"))
+            sendMessage(SendMessage(chatId, "Game finished"))
           } else {
-            sendMessage (SendMessage(chatId, "Only author of the game can finish it"))
+            sendMessage(SendMessage(chatId, "Only author of the game can finish it"))
           }
       case WinGame(chatId, snapshot) =>
         activeGames.get(chatId).foreach(_.game ! PoisonPill)
@@ -146,7 +159,7 @@ object Bot extends App with LazyLogging  {
       case MoveAction(chatId, userId, direction) =>
         activeGames.get(chatId) match {
           case Some(game) => game.game ! Move(userId, direction)
-          case None => sendMessage (SendMessage(chatId, "There is no games in this chat"))
+          case None => sendMessage(SendMessage(chatId, "There is no games in this chat"))
         }
     }
   }
@@ -154,7 +167,7 @@ object Bot extends App with LazyLogging  {
   case class Move(playerId: Int, direction: Direction)
 
   class GameMaster(chatId: Int, players: Set[Int]) extends Actor {
-    val game = new Game(SortedSet(players.toList:_*))
+    val game = new Game(SortedSet(players.toList: _*))
 
     override def receive: Receive = {
       case Move(playerId, direction) =>
@@ -174,9 +187,11 @@ object Bot extends App with LazyLogging  {
         for (_ <- message) TelegramApiClient sendMessage SendMessage(chatId, _)
     }
   }
+
 }
 
 object PropsStore {
+
   import resource._
 
   private val fileName = "props.store"
